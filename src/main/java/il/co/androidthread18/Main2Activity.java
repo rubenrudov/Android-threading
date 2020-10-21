78e37fa3487648c475ae17c241443258e56ee586
rubenrudov <59509727+rubenrudov@users.noreply.github.com>
2020-10-21 17:24:31 +0300

    Update Main2Activity.java

--- src/main/java/il/co/androidthread18/Main2Activity.java
+++ src/main/java/il/co/androidthread18/Main2Activity.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
+// Ruben Rudov
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
 
     TextView timeLeft, title;
